81e1fc0d6b792f8760e365493d2658e6698ee6ae
Ilia Nikolaenko <ilzen@mail.com>
2021-01-27 19:41:52 +0100

    Lesson 02 hometask v2 done, corrected variables names and new branch merge request

--- 02_variables-and-numbers/src/task01.js
+++ 02_variables-and-numbers/src/task01.js
@@ -1,11 +1,17 @@
 /* Задание 1 - площадь прямоугольника по координатам двух точек на его противоположных углах */
+
+/* Координаты точки 1 */
 let x1 = 2;
 let y1 = 7;
+
+/* Координаты точки 2 */
 let x2 = 5;
 let y2 = 5;
 
-let katheteX = Math.abs(x1 - x2);
-let katheteY = Math.abs(y1 - y2);
+/* Построение катетов прямоугольного треугольника */
+let cathetusX = Math.abs(x1 - x2);
+let cathetusY = Math.abs(y1 - y2);
 
-let flaecheRE = katheteX * katheteY;
-console.log('Площадь треугольника =', flaecheRE);
+/* Расчет площади прямоугольника по катетам треугольника */
+let squareAreaRectangle = cathetusX * cathetusY;
+console.log('Площадь треугольника =', squareAreaRectangle);
--- 02_variables-and-numbers/src/task03.js
+++ 02_variables-and-numbers/src/task03.js
@@ -1,9 +1,14 @@
 /* Генератор случайных нечетных чисел в интервале от любых n до любых m */
+
+/* Задаем произвольные значения m и n */
 let m = 3;
 let n = 100;
 
-let intervall = Math.abs(n - m);
-let startIntervall = Math.min(m, n);
+/* Находим величину интервала между m и n в абсолютном выражении + определяем начало интервала по минимальному значению */
+let intervalLength = Math.abs(n - m);
+let intervalStart = Math.min(m, n);
 
-let zufallzahl = Math.floor(((Math.random() * intervall + startIntervall) / 2)) * 2 + 1;
-console.log(zufallzahl);
+/* Расчет произвольного нечетного числа через умножение произвольного числа от 0 до 1 на величину интервала; прибавление начала интервала; Для получения строго нечетного числа деленим получившееся число из промежутка на 2; округляем его до минимального целого числа; умножаем на 2 ля получения четного числа и прибавляем 1. */
+let unevenRandomDigit = Math.floor(((Math.random() * intervalLength + intervalStart) / 2)) * 2 + 1;
+
+console.log(unevenRandomDigit);
