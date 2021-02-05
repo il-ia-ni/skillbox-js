7b71e945937a0a303463ea308cee21ac14bd6748
Ilia Nikolaenko <ilzen@mail.com>
2021-02-05 13:22:56 +0100

    Lesson 03 hometask v2 done - task 2 updated to the comments, less variables used and console.log is out of if-cycle now

--- 03_string-boolean-conditionals/src/task02.js
+++ 03_string-boolean-conditionals/src/task02.js
@@ -1,33 +1,23 @@
 /* Задание 2 - преобразование написания двух переменных с именем и фамилией в корректный регистр с выведением корректного написания имени и информации, было ли оно преобразовано */
 
-let defaultName = 'BenedicT';
-let defaultSurname = 'CumberBatch';
+let defaultName = 'GaBourEy';
+let defaultSurname = 'siDiBe';
 
-let tempNameFirstLetter = defaultName.substr(0, 1); // Извлекаем из строки имени подстроку начиная с индекса 0, длина извлечения - 1 символ
-let tempNameOtherLetters = defaultName.substr(1); // Извлекаем из строки имени подстроку начиная с индекса 1, длина извлечения - все символы до конца строки
-let tempSurnameFirstLetter = defaultSurname.substr(0, 1); // Извлекаем из строки имени подстроку начиная с индекса 0, длина извлечения - 1 символ
-let tempSurnameOtherLetters = defaultSurname.substr(1); // Извлекаем из строки имени подстроку начиная с индекса 1, длина извлечения - все символы до конца строки
-
-let capitalNameFirstLetter = tempNameFirstLetter.toUpperCase(); // имя - делаем первую букву прописной
-let smallNameOtherLetters = tempNameOtherLetters.toLowerCase(); // имя - делаем остальные буквы строчными
-let capitalSurnameFirstLetter = tempSurnameFirstLetter.toUpperCase(); // фамилия - делаем первую букву прописной
-let smallSurameOtherLetters = tempSurnameOtherLetters.toLowerCase(); // фамилия - делаем остальные буквы строчными
-
-let correctName = `${capitalNameFirstLetter}${smallNameOtherLetters}`;
-let correctSurname = `${capitalSurnameFirstLetter}${smallSurameOtherLetters}`;
+let correctName = `${(defaultName.substr(0, 1)).toUpperCase()}${(defaultName.substr(1)).toLowerCase()}`;
+let correctSurname = `${(defaultSurname.substr(0, 1).toUpperCase())}${(defaultSurname.substr(1)).toLowerCase()}`;
 
 console.log(correctName, correctSurname);
 
+let status = '';
+
 if (defaultName === correctName && defaultSurname === correctSurname) {
-    let status = 'Имя и фамилия остались без изменений';
-    console.log(status);
+    status = 'Имя и фамилия остались без изменений';
 } else if (defaultName === correctName && defaultSurname !== correctSurname) {
-    let status = 'Имя осталось без изменений, Фамилия была преобразована';
-    console.log(status);
+    status = 'Имя осталось без изменений, Фамилия была преобразована';
 } else if (defaultName !== correctName && defaultSurname === correctSurname) {
-    let status = 'Имя было преобразовано, Фамилия осталась без изменений';
-    console.log(status);
+    status = 'Имя было преобразовано, Фамилия осталась без изменений';
 } else if (defaultName !== correctName && defaultSurname !== correctSurname) {
-    let status = 'Имя и Фамилия были преобразованы';
-    console.log(status);
-}
+    status = 'Имя и Фамилия были преобразованы';
+} 
+
+console.log(status);
